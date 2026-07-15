71a97c5624c8c1ceca154e22f46d04f02b417c60
RichardSPoltronieri <tecnicorichard1@gmail.com>
2026-07-15 19:07:14 -0300

    Implementação de tratativa de erro e cenários adicionais

--- api/cypress/e2e/post.cy.js
+++ api/cypress/e2e/post.cy.js
@@ -10,13 +10,32 @@ describe('POST /api/users/register', () => {
     }
 
     cy.post(user).then((response) => {
-      
+
       expect(response.status).to.eq(201)
       expect(response.body.message).to.eq('User registered successfully.')
       expect(response.body.user.name).to.eq(user.name)
       expect(response.body.user.email).to.eq(user.email)
 
       cy.log(JSON.stringify(response.body))
+    })
+  })
+
+  it('Não deve cadastrar com email duplicado', () => {
+
+    const user = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: '123456'
+    }
+
+    cy.post(user).then((response) => {
+      expect(response.status).to.eq(201)
+    })
+
+    cy.post(user).then((response) => {
+
+      expect(response.status).to.eq(409)
+      expect(response.body.error).to.eq('Email already exists.')
     })
   })
 
@@ -34,14 +53,60 @@ describe('POST /api/users/register', () => {
     })
   })
 
+  it('O campo email deve ser obrigatório', () => {
+    const user = {
+      name: faker.person.fullName(),
+      password: '123456'
+    }
+
+    cy.post(user).then((response) => {
+
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+      expect(response.body.error).to.eq('Email is required.')
+    })
+  })
+
+  it('O campo senha deve ser obrigatório', () => {
+    const user = {
+      name: faker.person.fullName(),
+      email: faker.internet.email()
+    }
+
+    cy.post(user).then((response) => {
+
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+      expect(response.body.error).to.eq('Password is required.')
+    })
+  })
+
+  it('Não deve enviar um JSON inválido', () => {
+    const user = `{
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: '123456'
+    }`
+
+    cy.post(user).then((response) => {
+
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+      expect(response.body.error).to.eq('Invalid JSON payload.')
+    })
+  })
+
 
 })
 
 Cypress.Commands.add('post', (user) => {
   return cy.api({
-      method: 'POST',
-      url: 'http://localhost:3333/api/users/register',
-      body: user,
-      failOnStatusCode: false
-})
+    method: 'POST',
+    url: 'http://localhost:3333/api/users/register',
+    body: user,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    failOnStatusCode: false
   })
+})
