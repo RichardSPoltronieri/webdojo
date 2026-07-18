7834e53b54ac43d92fbf299dbc2d38c8b317a5f0
RichardSPoltronieri <tecnicorichard1@gmail.com>
2026-07-18 10:45:13 -0300

    feat: DELETE

--- api/index.js
+++ api/index.js
@@ -129,6 +129,16 @@ app.put('/api/users/:id', async (req, res) => {
     }
 
     try {
+
+        const user = await prisma.user.findUnique({
+            where: { id: Number(id) }
+        })
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' })
+        }
+
+
         await prisma.user.update({
             where: { id: Number(id) },
             data: { name, email, password }
@@ -141,6 +151,33 @@ app.put('/api/users/:id', async (req, res) => {
     }
 
 })
+
+app.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: { id: Number(id) }
+        })
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' })
+        }
+
+        await prisma.user.delete({
+            where: { id: Number(id) }
+        })
+
+        return res.status(204).end()
+
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete user.' })
+    }
+})
+
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
